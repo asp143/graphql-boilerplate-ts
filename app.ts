6bc45be49a627ca4ed56738effd67c09e638f193
asp143 <mungcs09@gmail.com>
2020-11-25 11:36:52 +0800

    Added Authentication

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import cookieParser from 'cookie-parser';
 import './src/utils/dbconnect';
+import { VerifyToken } from './src/utils/jwt';
 
 import schema from './src/graphql';
 import model from './src/models';
@@ -9,8 +10,9 @@ import model from './src/models';
 const app = express();
 const apolloServer = new ApolloServer({ 
     schema,
-    context: async () => {
-        return { model };
+    context: async ({ req }: { req: Request }) => {
+        const Auth = req.headers.authorization ? VerifyToken(req.headers.authorization) : null;
+        return { model, Auth };
     }
 });
 
